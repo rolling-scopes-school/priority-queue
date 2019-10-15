cb89feefee1229f90d3f444ebbed28d23133a1dc
romanrg <nekhaichyk.raman@gmail.com>
2019-10-15 19:18:10 +0300

    feat: add Node:#appendChild

--- src/node.js
+++ src/node.js
@@ -2,13 +2,17 @@ class Node {
 	constructor(data, priority) {
 		this.data = data;
 		this.priority = priority;
-		this.parent = null;
+		this.parent = null;		
 		this.left = null;
 		this.right = null;
 	}
 
-	appendChild(node) {
-
+	appendChild(node) {	
+		if (this.left && this.right) {
+			return
+		}	
+		this.left !== null ? this.right = node : this.left = node;
+		
 	}
 
 	removeChild(node) {
